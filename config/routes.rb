f149da749acb2612a53ed0f4db2ed91841b8db31
Hack Reactor Students <github@hackreactor.com>
2013-05-31 11:51:14 -0700

    completed first round of routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,17 @@ Clams::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+#  Sample resource route (maps HTTP verbs to controller actions automatically):
+  resources :users
+  resources :comments
+  resources :subscriptions
+  resources :delete
+  resources :status
+  resources :groups
+  resources :routes
+  resources :statuses
+  resources :posts
+
 
   # Sample resource route with options:
   #   resources :products do
